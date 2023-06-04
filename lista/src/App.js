d1596cffe9ad3a1b1e279d671d7c95e4d4fe1c45
samuel-eduardo <samuellino2006@gmail.com>
2023-06-04 16:36:41 -0300

    exercicio concluido

--- lista/src/App.js
+++ lista/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import carros from './objetos.js'
+import './app.css'
+import $ from 'jquery'
+const lista_de_carros = carros
 
 function App() {
+  function pesquisar(){
+    let ano = $('#ano').val()
+    let port = $('#porte').val()
+    let lista = []
+    var por = lista_de_carros.filter((p)=>{
+      if(ano === '' && port === ''){
+        return lista_de_carros
+      }else if(ano === '' && port !== ''){
+        return p.porte === port
+      }else if(port === '' && ano !== ''){
+        return p.ano === ano
+      }else{
+        return p.porte === port && p.ano === ano
+      }
+      
+    })
+    var res
+    if(por.length === 0){
+        $('.mostrar').html('<h3>Nada encontrado</h3>')
+      }else{
+        res = por
+        for(var ele of res){
+          if(lista.includes(ele) === true){
+            return
+          }else{
+            lista.push(ele)
+          }
+        }
+        console.log(lista)
+        $('.mostrar').html('')
+        for(var obj of lista){
+          $('.mostrar').append('<label>'+ obj.modelo +'</label>')
+        }}
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="filtro">
+        <h1>Filtro</h1>
+        <label form='porte'>porte</label>
+        <select id='porte'>
+          <option value={''}></option>
+          <option value={'médio'}>médio</option>
+          <option value={'compacto'}>compacto</option>
+        </select>
+        <label form='ano'>Ano</label>
+        <select id='ano'>
+          <option value={''}></option>
+          <option value={'2023'}>2023</option>
+          <option value={'2024'}>2024</option>
+        </select>
+      </div>
+      <div className="res">
+        <button onClickCapture={pesquisar}>pesquisar</button>
+
+      </div>
+      <div className="mostrar">
+
+      </div>
     </div>
   );
 }
