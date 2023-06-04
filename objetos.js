2e54732704a1e13c70de0660036e0059b2a51f18
samuel-eduardo <samuellino2006@gmail.com>
2023-06-04 14:11:35 -0300

    add react

---
+++ objetos.js
@@ -0,0 +1,7 @@
+const carros = [{fabricante: 'GMW', modelo: 'Haval H6 Premiun 1.5 PHEV', ano: '2024', preço: 269000, porte: 'médio', lugares: 5, portas: 4},
+{fabricante: 'Volkswagen', modelo: 'Virtus 1.0 TSi', ano: '2023', preço: 104390, porte: 'compacto', lugares: 5, portas: 4},
+{fabricante: 'Fiat', modelo: 'Pulse Abarth 1.3 Turbo', ano: '2023', preço: 149990, porte: 'compacto', lugares: 5, portas: 4},
+{fabricante: 'Hyundai', modelo: 'HB20 Confort 1.0 turbo', ano: '2024', preço: 98990, porte: 'compacto', lugares: 5, portas: 4},
+{fabricante: 'Volkswagen', modelo: 'Polo 1.0 TSi', ano: '2023', preço: 95990, porte: 'compacto', lugares: 5, portas: 4},
+{fabricante: 'Fiat', modelo: 'Pulse Drive 1.0 turbo', ano: '2023', preço: 110790, porte: 'compacto', lugares: 5, portas: 4}]
+export default carros
